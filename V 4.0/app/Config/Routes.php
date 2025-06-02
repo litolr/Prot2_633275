1ddb4f941df4c0c65d1307276496b15d51599a69
litolr <litoitu@gmail.com>
2025-06-01 22:03:53 -0300

    Update Routes.php

--- V 4.0/app/Config/Routes.php
+++ V 4.0/app/Config/Routes.php
@@ -5,10 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+if (!isset($routes) || !$routes instanceof RouteCollection) {
+	$routes = service('routes');
+}
 $routes->get('/', 'Home::index');
-$routes->get('principal', 'Home::principal');
+$routes->get('principal', 'Home::index');
 $routes->get('quienes_somos', 'Home::quienes_somos');
+$routes->get('registro', 'Home::registro');
 $routes->get('acercade', 'Home::acercade');
 $routes->get('login', 'Home::login');
-$routes->get('registro', 'Home::registro');
 
